af53391c28f12d9328f38d9b068a6f54c1c19c7c
FotisFarm <f.fotis.00@gmail.com>
2023-08-14 18:29:53 +0300

    Update Exercise3

---
+++ Exercise_3/Ex3/Ex3.js
@@ -0,0 +1,37 @@
+import {Octokit} from "@octokit/rest";
+import fs from 'fs';
+import {createObjectCsvWriter} from 'csv-writer'
+
+
+/*Create an Octokit object and authenticate using a personal token in order to make the RESTfull API call */
+const Ex3 = new Octokit ({
+    auth: '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',
+});
+
+/*Create the csv file */  
+const csvfile =  createObjectCsvWriter({
+    path : "Ex3.csv",
+    header: [
+        {id : 'name', title: 'Name'},
+        {id : 'full_name', title: 'Full_name'},
+        {id : 'id', title: 'Id'}
+    ],
+})
+
+/* Search for all repos of a Github account (in this instance, my own) */
+const data = await Ex3.rest.search.repos ({
+    q : 'user:FotisFarm'
+})  
+
+/* Get the name, full name and id of each repository */
+const items = data.data.items;
+var write = [ ];
+for (var i of items){
+    
+    write.push( {name : i.name ,full_name : i.full_name, id : i.id} ); 
+    
+   
+}
+
+csvfile.writeRecords(write)
+
