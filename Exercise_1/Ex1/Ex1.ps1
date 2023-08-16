ee89fab1f3182f757a3602c70e2beccfbdaa535f
FotisFarm <f.fotis.00@gmail.com>
2023-08-16 11:02:40 +0300

    Added comments to Ex1 and Ex2

--- Exercise_1/Ex1/Ex1.ps1
+++ Exercise_1/Ex1/Ex1.ps1
@@ -1,12 +1,14 @@
+#Get path of working directory
 $sample = (Get-item $PSCommandPath).DirectoryName
-
 Set-Location $sample
+
+#Create txt file for writing the fullpaths
 New-Item -Path $sample -Name "Ex1.txt"
 
-
+#Recursively search in the given directories for directories named 'Action#'
 $a = Get-ChildItem -Path (Join-path $sample 'Action[0-9]') -Recurse -Directory
  
-
+#In each directory search for files named 'Script.mts'
 foreach ($b in $a) {
     $c = Get-ChildItem -filter "Script.mts" -path $b
     if ($null -eq $c) { 
--- Exercise_2/Ex2/Ex2.py
+++ Exercise_2/Ex2/Ex2.py
@@ -5,18 +5,21 @@ import csv
 ex2 = open('Ex2.csv','w')
 writer = csv.writer(ex2,delimiter=';')
 
+#Search for xml files in this directory
 for files in os.listdir():
     csvwrite = []
     if files.endswith('.xml'):
        
+       #Iterate through elements to find those with tag: 'inputSymbol'
         filexml = ET.parse(files)
         root = filexml.getroot()
         for child in root:
-            for i in child.findall('Items'):
-                for childi in i:
-                    input = childi.find('inputSymbol')
+            for items in child.findall('Items'):
+                #'inputSymbol' elements are children of 'Item' elements, who are children of 'Items' elements 
+                for item in items:
+                    input = item.find('inputSymbol')
                     if input is not None and input.text == '_pdg_list':
-                        value = childi.find('value').text
+                        value = item.find('value').text
                         csvwrite.append(value)
     if csvwrite:
         csvwrite.insert(0,os.path.abspath(files))
